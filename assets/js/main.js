fb66acd714cf0323d6c8813e1fcee6c21ae7c922
marwanabi <nabimarwa@gmail.com>
2021-12-28 02:20:39 +0600

    remove slick on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,51 +15,13 @@ function scrollID() {
 $(document).ready(function() {
   scrollID();
 
-  const slider = $('.slider');
-
-  function onSliderAfterChange(event, slick, currentSlide) {
-    $(event.target).data('current-slide', currentSlide);
-  }
-
-  function onSliderWheel(e) {
-    var deltaY = e.originalEvent.deltaY,
-      $currentSlider = $(e.currentTarget),
-      currentSlickIndex = $currentSlider.data('current-slide') || 0;
-
-    if (
-      // check when you scroll up
-      (deltaY < 0 && currentSlickIndex == 0) ||
-      // check when you scroll down
-      (deltaY > 0 && currentSlickIndex == $currentSlider.data('slider-length') - 1)
-    ) {
-      return;
-    }
-
-    e.preventDefault();
-
-    if (e.originalEvent.deltaY < 0) {
-      $currentSlider.slick('slickPrev');
-    } else {
-      $currentSlider.slick('slickNext');
-    }
-  }
-
-  slider.each(function(index, element) {
-    var $element = $(element);
-    // set the length of children in each loop
-    // but the better way for performance is to set this data attribute on the div.slider in the markup
-    $element.data('slider-length', $element.children().length);
-  })
-  .slick({
-    infinite: false,
+  $('.slider').slick({
+    dots: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    dots: true,
     arrows: false,
-    fade: true
-  })
-  .on('afterChange', onSliderAfterChange)
-  .on('wheel', onSliderWheel);
+    fade: true,
+  });
 
   $(document).scroll(function() {
     checkOffset();
